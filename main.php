ac9b3a8f9ba065b13d1c9a6c8c96c9c11ac5fb53
2232017account <s2232017qp@s.chibakoudai.jp>
2024-11-05 01:42:16 +0900

    Add files via upload

---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>スロットゲーム</title>
+</head>
+
+<body>
+	<h1>大学生のスロット生活</h1>
+	<hr color="red">
+	<form action="start.php" method="get" />
+  	<input type="submit" value="スタート" />
+	</form>
+	<hr color="red">
+	<form action="rule.php" method="get" />
+  	<input type="submit" value="あそびかた">
+	<hr color="red">
+	</form>
+</body>
+</html>
